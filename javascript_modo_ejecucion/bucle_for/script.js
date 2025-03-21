918e7daaa63871a57a6cd0f196465fa98774c756
andartesp <andartesp0120@gmail.com>
2025-03-21 12:30:29 +0100

    21/03/2025

---
+++ javascript_modo_ejecucion/bucle_for/script.js
@@ -0,0 +1,18 @@
+const temperaturas = [
+    [23,25,22,16,24,22,27],
+    [33,35,32,36,34,32,37],
+    [13,15,12,26,14,12,17],
+    [25,25,25,25,25,25,25]]; 
+
+const localidades = ["Vera","Huércal-Overa","Albox","Mojácar"];
+
+const data = document.getElementById("localidad");
+
+for(let i=0; i<localidades.length;i++){
+    data.innerHTML = data.innerHTML + `<option value="${localidades[i]}">${localidades[i]}</option>`
+}
+
+function avglocalidad(){
+document.getElementById("res_avglocalidad").innerHTML="jajaja";
+document.getElementById("localidades").value
+}
